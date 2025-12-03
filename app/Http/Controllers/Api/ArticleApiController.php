29b63f19739329e591e18383f0fde8482ad00b4b
aldinpramudya <aldinarielpramudya@gmail.com>
2025-12-03 09:43:10 +0700

    feat(API) : Public Api for Artikel

--- app/Http/Controllers/Api/ArticleApiController.php
+++ app/Http/Controllers/Api/ArticleApiController.php
@@ -3,9 +3,51 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::with('tags')
+            ->where('is_visible', true)
+            ->latest()
+            ->paginate(10);
+
+        // Transform data untuk menambahkan full URL pada gambar
+        $articles->getCollection()->transform(function ($article) {
+            $article->article_image = $article->article_image
+                ? url('storage/' . $article->article_image)
+                : null;
+            return $article;
+        });
+
+        return response()->json([
+            "message" => "Data Articles Shown",
+            "data" => $articles,
+        ], 200);
+    }
+
+    public function show($slug)
+    {
+        $article = Article::with('tags')->where('article_slug', $slug)->where('is_visible', true)->first();
+
+        if (!$article) {
+            return response()->json([
+                "message" => "Article Not Found",
+                "data" => null,
+            ], 404);
+        }
+
+        // Transform image URL
+        $article->article_image = $article->article_image
+            ? url('storage/' . $article->article_image)
+            : null;
+
+        return response()->json([
+            "message" => "Article Detail Shown",
+            "data" => $article,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/articles', [ArticleApiController::class, 'index']);
+Route::get('/articles/{slug}', [ArticleApiController::class, 'show']);
